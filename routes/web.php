11f78e1c91fb90d555a13280d3d6fee0e0e72ed0
twdavidong <64457327+twdavidong@users.noreply.github.com>
2022-05-04 03:25:00 +0800

    Commit2

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile'); // view profile
-
-Route::get('/profile/create', [App\Http\Controllers\ProfileController::class, 'create']); // create profile
-Route::get('/profile/edit', [App\Http\Controllers\ProfileController::class, 'edit']);  // edit profile 
-
-Route::post('/profile/postCreate', [App\Http\Controllers\ProfileController::class, 'postCreate']); // create post ->name('profile.postCreate');
-Route::post('/profile/{id}/postEdit', [App\Http\Controllers\ProfileController::class, 'postEdit']); // edit post ->name('profile.postEdit');
+Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile');
+Route::get('/profile/create', [App\Http\Controllers\ProfileController::class, 'create']);
+Route::post('/profile/postCreate', [App\Http\Controllers\ProfileController::class, 'postCreate'])->name('profile.postCreate');
+Route::get('/profile/edit', [App\Http\Controllers\ProfileController::class, 'edit']);
+Route::post('/profile/{id}/postEdit', [App\Http\Controllers\ProfileController::class, 'postEdit'])->name('profile.postEdit');
 
 Route::resource('post', App\Http\Controllers\PostController::class);
-
-Route::post('/post/showPosts', [App\Http\Controllers\PostController::class, 'showPosts']);  // show all posts 
